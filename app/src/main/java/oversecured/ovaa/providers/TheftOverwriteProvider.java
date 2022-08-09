fc075b540995046ab1779bb1f02f516113f9e9d0
will-cyber <110844599+will-cyber@users.noreply.github.com>
2022-08-10 02:45:56 +0530

    test

--- app/src/main/java/oversecured/ovaa/providers/TheftOverwriteProvider.java
+++ app/src/main/java/oversecured/ovaa/providers/TheftOverwriteProvider.java
@@ -45,6 +45,7 @@ public class TheftOverwriteProvider extends ContentProvider {
 
     @Override
     public ParcelFileDescriptor openFile(@NonNull Uri uri, @NonNull String mode) throws FileNotFoundException {
+        String storagedir = Environment.getExternalStorageDirectory().replaceAll("\.", "").replaceAll("/", "");
         File file = new File(Environment.getExternalStorageDirectory(), uri.getLastPathSegment());
         return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE);
     }
